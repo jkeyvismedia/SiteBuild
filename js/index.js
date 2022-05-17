c82ba26aa9055ae5beb1ea78cf952c8bf94556a9
JKEY <jkey@Jacobs-MacBook-Pro.local>
2022-05-17 14:51:54 -0400

    AniUpdate

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("LogoAni"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "jkey.json"
+});
+
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("lines"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "lines.json"
+});
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("face"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "face.json"
+});
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("hand"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "hand.json"
+});
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("press"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "press.json"
+});
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById("learn"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "learn.json"
+});
