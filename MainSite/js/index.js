b128da32ab30035c8be353a369a0ff6b82d7601c
JKEY <jkey@Jacobs-MacBook-Pro.local>
2022-06-15 21:44:09 -0400

    addedSite

---
+++ MainSite/js/index.js
@@ -0,0 +1,22 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById("LogoAni"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "media/jkey.json"
+});
+
+window.onscroll = () => { window.scroll(0, 0); };
+
+var a;
+function reveal() {
+  if (a==1){
+    document.getElementById("OV" & "socials").style.display="inline";
+    return a=0;
+  }
+  else {
+    document.getElementById("OV" & "socials").style.display="none";
+    return a=1;
+  }
+}
+
